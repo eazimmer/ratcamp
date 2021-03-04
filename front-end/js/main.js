871d224cd8272c8810d558a6d3a31c706e63b304
Eric <eric.zimmer@mymail.champlain.edu>
2021-03-04 11:30:33 -0700

    Resolve username transmission issue

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -5,6 +5,7 @@ const setUsername = () => {
   console.log("setUsername entered")
   username = document.getElementById('username-input').value;
   console.log("Username stored: " + username)
+  location.href = "messageBoard.html";
 }
 
 $(document).ready(function() {
