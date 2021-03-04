3ebe42cea907cbba03759bca2739e1e5e7291427
Eric <eric.zimmer@mymail.champlain.edu>
2021-03-04 11:01:51 -0700

    Debugging

--- back-end/index.js
+++ back-end/index.js
@@ -13,11 +13,6 @@ const router = app => {
         response.sendFile(__dirname + "/front-end/html/index.html");
     });
 
-    app.get('/front-end/html/messageBoard.html', (request, response) => {
-        console.log("Inside app GET end-point")
-        response.sendFile(__dirname + "/front-end/html/messageBoard.html");
-    });
-
     app.post('/front-end/html/messageBoard.html', (request, response) => {
         console.log("Inside app POST end-point")
         response.sendFile(__dirname + "/front-end/html/messageBoard.html");
