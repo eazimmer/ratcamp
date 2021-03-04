d48f7992dc6fd1a7dc24521851ce430d87408c37
Eric <eric.zimmer@mymail.champlain.edu>
2021-03-04 09:13:10 -0700

    Channge served HTML file via app.get('/') endpoint

--- back-end/index.js
+++ back-end/index.js
@@ -10,9 +10,7 @@ const router = app => {
         //     message: 'Node.js and Express REST API for CSI330-Group2'
         // });
 
-        response.sendFile(__dirname + "/front-end/html/messageBoard.html");
-
-
+        response.sendFile(__dirname + "/front-end/html/index.html");
     });
 
     app.get('/front-end/*', (req, res) => {
