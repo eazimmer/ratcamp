92adec89b7be891807a6afad509a35e022d2c002
Eric Zimmer <eric.zimmer@mymail.champlain.edu>
2021-03-05 14:01:35 -0700

    Clean up backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,7 @@
 // http://localhost:3002/
-// Access this link to hit the locally-running-server's homepage
+// Use this to hit the locally-running-server's homepage
 
-// https://csi330-group2.herokuapp.com/
-// Live Heroku server
-
-// Require packages and set the port
+// Required packages and set up ports for local hosting
 const express = require('express');
 const bodyParser = require('body-parser');
 const service = require('./index.js');
@@ -13,76 +10,115 @@ const port = process.env.PORT || 3002;
 const host = '0.0.0.0';
 const path = require('path');
 
-let messages = [];
-let users = {};
 
-let sockets_to_names = [];
+// Stored data
+let messages = []; // Objects representing messages containing "id", "msg", and "name"
+let users_to_message_ids = {}; // Mapping of users to their respective message id counts
+let sockets_to_names = []; // List of maps of socket ids to usernames: "id", and "name" keys
 
-// Use Node.js body parsing middleware
+
+// Use Node.js body parsing middleware to help access message contents
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-service(app);
 
-app.use(express.static(path.join(__dirname + '/front-end')));
+service(app); // Link endpoints page
+app.use(express.static(path.join(__dirname + '/front-end'))); // Establish working directory in filesystem
+
 
 // Start the server
 const server = app.listen(port, host, (error) => {
   if (error) return console.log(`Error: ${error}`);
-
   console.log(`Server listening on port ${server.address().port}`);
 });
 
 
-
+// Initialize socket.io object linked to now-live server
 const io = require('socket.io')(server);
 
-function addMsg(name, msg) {
-  if (!(name in users)) {
-    users[name] = {};
-    users[name]['nextmsgid']= 0;
+
+// Handle messages received by server
+function registerMessage(user, msg) {
+
+  // Begin tracking new user and their respective message ids
+  if (!(user in users_to_message_ids)) {
+    users_to_message_ids[user] = {};
+    users_to_message_ids[user]['nextmsgid']= 0;
   }
-  let data = { id: (name + '-' + users[name].nextmsgid), msg: msg, name: name };
+
+  // Increment user's message id count
+  users_to_message_ids[user].nextmsgid += 1;
+
+  // Construct message object
+  let data = {
+    id: (user + '-' + users_to_message_ids[user].nextmsgid),
+    msg: msg,
+    name: user
+  };
+
+  // Update list of messages
   messages.push(data);
-  io.emit('msgrecv', JSON.stringify(data));
-  users[name].nextmsgid += 1;
+
+  // Broadcast message to clients
+  broadcastMessage(data);
 }
 
-function onlineUsersChanged() {
+
+// Send message received by server to all clients
+function broadcastMessage(data_object) {
+  io.emit('msgrecv', JSON.stringify(data_object));
+}
+
+
+// Update clients of change in online users
+function broadcastChangeInOnlineUsers() {
   let validated_users = []
 
+  // Pull currently online users out of map of socket ids to usernames
   for (var i in sockets_to_names) {
     validated_users.push(sockets_to_names[i]["name"])
   }
 
+  // Broadcast new list of online users to all clients
   io.emit('updateonlineusers', validated_users);
 }
 
+
+// Socket connection event
 io.on('connection', socket => {
 
+  // Endpoint handling incoming message
   socket.on('chat', message => {
     let data = JSON.parse(message);
-    addMsg(data.name, data.msg);
+    registerMessage(data.name, data.msg);
   });
 
+
+  // Endpoint registering new connection with a chosen username
   socket.on('login-name', name => {
     var socket_id = socket.id.toString()
+
     sockets_to_names.push({
       "id" : socket_id,
       "name" : name
     })
-    onlineUsersChanged()
+
+    broadcastChangeInOnlineUsers() // Update clients with new online user list
   });
 
+
+  // Endpoint handling disconnects
   socket.on("disconnect", () => {
+
+    // Find disconnecting socket and remove its entry in socket -> user map
     for (var i in sockets_to_names) {
       if (sockets_to_names[i]["id"] === socket.id) {
         sockets_to_names.splice(i, 1)
       }
     }
-    onlineUsersChanged()
-  });
 
+    broadcastChangeInOnlineUsers() // Update clients with new online user list
+  });
 })
--- back-end/index.js
+++ back-end/index.js
@@ -1,41 +1,24 @@
-// Persistently stored data
-
-
 // Endpoint Routing
 const router = app => {
 
-    // Homepage
+    // Serve clients with homepage
     app.get('/', (request, response) => {
-        // response.send({
-        //     message: 'Node.js and Express REST API for CSI330-Group2'
-        // });
         response.sendFile(__dirname + "/front-end/html/index.html");
     });
 
+    // CURRENTLY UNUSED
+    // Serve client with varying file based on URL.
+    app.get('/front-end/*', (req, res) => {
+        res.sendFile(__dirname + req.originalUrl);
+    });
+
+    // Serve clients with message board
     app.get('/front-end/html/messageBoard.html', (request, response) => {
         response.sendFile(__dirname + "/front-end/html/messageBoard.html");
     });
 
-    app.get('/front-end/*', (req, res) => {
-        console.log(req.originalUrl);
-        res.sendFile(__dirname + req.originalUrl);
-    });
-
-    // ENDPOINT POST: Send a message from client to server
-    app.post('/message', (request, response) => {
-        const message = request.body;
-
-        let user = message["Username"]
-        let message_contents = message["Message"]
-
-        users.push(user)
-        messages.push(message_contents)
-
-        response.send("User: " + user + ", sent: " + message_contents)
-    });
-
-
-    // ENDPOINT GET: Receive all messages from server
+    // CURRENTLY UNUSED
+    // Receive all messages from server. Currently unused.
     app.get('/messages', (request, response) => {
         response.json(messages)
     });
